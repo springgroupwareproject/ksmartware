b264f3ebdd8bc8f8487f78ea1d75c6fe11cfb0cd
Administrator <Administrator@smart20113>
2019-06-26 09:05:23 +0900

    [개발자 31기 안찬솔 2019-06-26]
    파일 순정으로 만들기

--- ksmartware/src/main/java/ksmart31/team01/admin/mapper/PersonnelSetUpMapper.java
+++
@@ -1,8 +0,0 @@
-package ksmart31.team01.admin.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PersonnelSetUpMapper {
-
-}
--- ksmartware/src/main/java/ksmart31/team01/index/mapper/IndexMapper.java
+++
@@ -1,8 +0,0 @@
-package ksmart31.team01.index.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface IndexMapper {
-
-}
