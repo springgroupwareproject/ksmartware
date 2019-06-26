844b83b03f0ca00eaf401ba6de53d9b7dfa774ea
Administrator <Administrator@MSDN-SPECIAL>
2019-06-26 16:57:28 +0900

    병합 작업

--- ksmartware/src/main/java/ksmart31/team01/accounting/controller/RestCustomerController.java
+++ ksmartware/src/main/java/ksmart31/team01/accounting/controller/RestCustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ksmart31.team01.accounting.domain.Customer;
+import ksmart31.team01.accounting.domain.Member;
 import ksmart31.team01.accounting.service.CustomerService;
 
 
--- ksmartware/src/main/java/ksmart31/team01/accounting/service/CustomerService.java
+++ ksmartware/src/main/java/ksmart31/team01/accounting/service/CustomerService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ksmart31.team01.accounting.domain.Customer;
 import ksmart31.team01.accounting.domain.Member;
 import ksmart31.team01.accounting.mapper.CustomerMapper;
 
