c04596374fad15a7258d1520f0c74ca681f63ae7
Administrator <Administrator@MSDN-SPECIAL>
2019-06-27 14:09:26 +0900

    [2019-06-27 이승진] 문서 관리 문서 데이터 관리 컨트롤러,서비스,매퍼,뷰 기본작업

---
+++ ksmartware/src/main/java/ksmart31/team02/document/domain/DocumentForm.java
@@ -0,0 +1,20 @@
+package ksmart31.team02.document.domain;
+
+public class DocumentForm {
+	private String documentFormName;
+
+	public String getDocumentFormName() {
+		return documentFormName;
+	}
+
+	public void setDocumentFormName(String documentFormName) {
+		this.documentFormName = documentFormName;
+	}
+
+	@Override
+	public String toString() {
+		return "DocumentForm [documentFormName=" + documentFormName + "]";
+	} 
+	
+	
+}
